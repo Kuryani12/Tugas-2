786201bf9c4851ff84d2578c24df0b2be38bec17
Kuryani12 <kuryaniani74@gmail.com>
2024-09-16 22:05:52 +0800

    mendeteksiganjilgenap.py

---
+++ mendeteksiganjilgenap.py
@@ -0,0 +1,7 @@
+number = int(input("Masukkan sebuah bilangan: "))
+
+# Mengecek apakah bilangan genap atau ganjil
+if number % 2 == 0:
+    print(f"{number} adalah bilangan genap.")
+else:
+    print(f"{number} adalah bilangan ganjil.")
